4f6e827e12b7b26e606ddc60f48b6ec3abd63c91
Nguyễn Bá Tuấn Anh <anhnbt.it@gmail.com>
2021-04-19 20:54:19 +0700

    Initial commit

---
+++ MusicManager/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace MusicManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAlbumName = new System.Windows.Forms.Label();
+            this.txtAlbumName = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnAdd2 = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.lstSelectedSongs = new System.Windows.Forms.ListBox();
+            this.lstAvailableSongs = new System.Windows.Forms.ListBox();
+            this.cboArtist = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblAlbumName
+            // 
+            this.lblAlbumName.AutoSize = true;
+            this.lblAlbumName.Location = new System.Drawing.Point(12, 9);
+            this.lblAlbumName.Name = "lblAlbumName";
+            this.lblAlbumName.Size = new System.Drawing.Size(67, 13);
+            this.lblAlbumName.TabIndex = 0;
+            this.lblAlbumName.Text = "Album Name";
+            this.lblAlbumName.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtAlbumName
+            // 
+            this.txtAlbumName.Location = new System.Drawing.Point(106, 6);
+            this.txtAlbumName.Name = "txtAlbumName";
+            this.txtAlbumName.Size = new System.Drawing.Size(457, 20);
+            this.txtAlbumName.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAdd2);
+            this.groupBox1.Controls.Add(this.btnRemove);
+            this.groupBox1.Controls.Add(this.lstSelectedSongs);
+            this.groupBox1.Controls.Add(this.lstAvailableSongs);
+            this.groupBox1.Controls.Add(this.cboArtist);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 38);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(551, 320);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Select Songs";
+            // 
+            // btnAdd2
+            // 
+            this.btnAdd2.Location = new System.Drawing.Point(238, 169);
+            this.btnAdd2.Name = "btnAdd2";
+            this.btnAdd2.Size = new System.Drawing.Size(75, 40);
+            this.btnAdd2.TabIndex = 7;
+            this.btnAdd2.Text = ">";
+            this.btnAdd2.UseVisualStyleBackColor = true;
+            this.btnAdd2.Click += new System.EventHandler(this.btnAdd2_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(238, 215);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 40);
+            this.btnRemove.TabIndex = 6;
+            this.btnRemove.Text = "<";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // lstSelectedSongs
+            // 
+            this.lstSelectedSongs.FormattingEnabled = true;
+            this.lstSelectedSongs.Location = new System.Drawing.Point(319, 97);
+            this.lstSelectedSongs.Name = "lstSelectedSongs";
+            this.lstSelectedSongs.Size = new System.Drawing.Size(226, 212);
+            this.lstSelectedSongs.TabIndex = 5;
+            // 
+            // lstAvailableSongs
+            // 
+            this.lstAvailableSongs.FormattingEnabled = true;
+            this.lstAvailableSongs.Location = new System.Drawing.Point(6, 97);
+            this.lstAvailableSongs.Name = "lstAvailableSongs";
+            this.lstAvailableSongs.Size = new System.Drawing.Size(226, 212);
+            this.lstAvailableSongs.TabIndex = 4;
+            // 
+            // cboArtist
+            // 
+            this.cboArtist.FormattingEnabled = true;
+            this.cboArtist.Location = new System.Drawing.Point(94, 31);
+            this.cboArtist.Name = "cboArtist";
+            this.cboArtist.Size = new System.Drawing.Size(140, 21);
+            this.cboArtist.TabIndex = 3;
+            this.cboArtist.SelectionChangeCommitted += new System.EventHandler(this.cboArtist_SelectionChangeCommitted);
+            this.cboArtist.SelectedValueChanged += new System.EventHandler(this.cboArtist_SelectedValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(316, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Selected Songs";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Available Songs";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Artist";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(407, 364);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(488, 364);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Cancel";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 399);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtAlbumName);
+            this.Controls.Add(this.lblAlbumName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAlbumName;
+        private System.Windows.Forms.TextBox txtAlbumName;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAdd2;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.ListBox lstSelectedSongs;
+        private System.Windows.Forms.ListBox lstAvailableSongs;
+        private System.Windows.Forms.ComboBox cboArtist;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ MusicManager/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace MusicManager
+{
+    public partial class Form1 : Form
+    {
+
+        private string connectionString = "server=LAB35\\SQL2012;database=MusicManager;uid=lab;pwd=";
+        private DataTable dtAvailableSongs = new DataTable();
+        private DataTable dtSelectedSongs = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.loadArtist();
+            this.loadSelectedSongs();
+        }
+
+        private void loadArtist()
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(connectionString);
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+                string queryString = "SELECT * FROM Artists";
+                SqlDataAdapter da = new SqlDataAdapter(queryString, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                cboArtist.DataSource = dt;
+                cboArtist.DisplayMember = "ArtistName";
+                cboArtist.ValueMember = "ArtistID";
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void loadAvailableSongs()
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(connectionString);
+                string queryString = "SELECT SongID, (CONVERT(NVARCHAR(4), SongID) + ' - ' + SongName) AS SongName FROM Songs WHERE ArtistID = " + cboArtist.SelectedValue;
+                SqlDataAdapter da = new SqlDataAdapter(queryString, conn);
+                dtAvailableSongs = new DataTable();
+                da.Fill(dtAvailableSongs);
+                lstAvailableSongs.DataSource = dtAvailableSongs;
+                lstAvailableSongs.DisplayMember = "SongName";
+                lstAvailableSongs.ValueMember = "SongID";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void loadSelectedSongs()
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(connectionString);
+                string queryString = "SELECT SongID, (CONVERT(NVARCHAR(4), SongID) + ' - ' + SongName) AS SongName FROM Songs WHERE 1 > 2";
+                SqlDataAdapter da = new SqlDataAdapter(queryString, conn);
+                dtSelectedSongs = new DataTable();
+                da.Fill(dtSelectedSongs);
+                lstSelectedSongs.DataSource = dtSelectedSongs;
+                lstSelectedSongs.DisplayMember = "SongName";
+                lstSelectedSongs.ValueMember = "SongID";
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void cboArtist_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            this.loadAvailableSongs();
+        }
+
+        private void cboArtist_SelectedValueChanged(object sender, EventArgs e)
+        {
+            this.loadAvailableSongs();
+        }
+
+        private void btnAdd2_Click(object sender, EventArgs e)
+        {
+            if (dtAvailableSongs.Rows.Count > 0)
+            {
+                DataRow dataRow = dtSelectedSongs.NewRow();
+                dataRow["SongID"] = lstAvailableSongs.SelectedValue;
+                dataRow["SongName"] = lstAvailableSongs.Text;
+                dtSelectedSongs.Rows.Add(dataRow);
+                dtSelectedSongs.AcceptChanges();
+
+                foreach (DataRow dr in dtAvailableSongs.Rows)
+                {
+                    if (dr["SongID"].ToString() == dataRow["SongID"].ToString())
+                    {
+                        dr.Delete();
+                    }
+                }
+                dtAvailableSongs.AcceptChanges();
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (dtSelectedSongs.Rows.Count > 0)
+            {
+                DataRow dataRow = dtAvailableSongs.NewRow();
+                dataRow["SongID"] = lstSelectedSongs.SelectedValue;
+                dataRow["SongName"] = lstSelectedSongs.Text;
+                dtAvailableSongs.Rows.Add(dataRow);
+                dtAvailableSongs.AcceptChanges();
+
+                foreach (DataRow dr in dtSelectedSongs.Rows)
+                {
+                    if (dr["SongID"].ToString() == dataRow["SongID"].ToString())
+                    {
+                        dr.Delete();
+                    }
+                }
+                dtSelectedSongs.AcceptChanges();
+            }
+        }
+    }
+}
